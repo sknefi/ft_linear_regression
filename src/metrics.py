a08030219ec45d18950d198685244f09a8f5d57b
sknefi <filip.karika1@gmail.com>
2026-06-19 22:29:37 +0200

    bonus files structure added

---
+++ src/metrics.py
@@ -0,0 +1,5 @@
+"""Bonus precision program.
+
+This file will compare predictions with real prices and print metrics such as
+MAE, RMSE, and R2.
+"""
---
+++ src/plot.py
@@ -0,0 +1,5 @@
+"""Bonus plotting program.
+
+This file will draw the dataset points and the trained regression line on the
+same graph.
+"""
